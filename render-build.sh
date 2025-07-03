39f7ec35b3d48d79b49859f81894c8962e3bdd80
MuthuNivedha R <muthunivedha135@gmail.com>
2025-07-03 20:20:57 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,5 @@
-apt-get update && apt-get install -y wkhtmltopdf
+mkdir -p bin
+curl -L -o bin/wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox-0.12.6-1_linux-generic-amd64.tar.xz
+tar -xf bin/wkhtmltopdf.tar.xz --strip-components=2 -C bin wkhtmltox/bin/wkhtmltopdf
+chmod +x bin/wkhtmltopdf
+rm bin/wkhtmltopdf.tar.xz
