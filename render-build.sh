b7813dc2f28f87bcdc7c7c6526ebf03080af4a3f
NIVEDHA-STAR <muthunivedha135@gamil.com>
2025-07-03 21:06:13 +0530

    Added gunicorn for Render deployment

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 
+# Download wkhtmltopdf precompiled binary
 mkdir -p bin
-curl -L -o bin/wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox-0.12.6-1_linux-generic-amd64.tar.xz
-tar -xf bin/wkhtmltopdf.tar.xz --strip-components=2 -C bin wkhtmltox/bin/wkhtmltopdf
-chmod +x bin/wkhtmltopdf
-rm bin/wkhtmltopdf.tar.xz
+curl -L -o bin/wkhtmltopdf https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltopdf_0.12.6-1.buster_amd64.deb
+
+# Install .deb and extract binary
+sudo apt-get update && sudo apt-get install -y gdebi-core
+sudo gdebi -n bin/wkhtmltopdf
+
+# Make it executable
+chmod +x /usr/local/bin/wkhtmltopdf
