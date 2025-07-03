b7f35f659fc2a2b87ace7e412e672a5c8b37bd4d
NIVEDHA-STAR <muthunivedha135@gamil.com>
2025-07-03 19:35:15 +0530

    Fix build script: use static wkhtmltopdf binary

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# Prepare bin folder
 mkdir -p bin
-curl -L -o bin/wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox-0.12.6-1_linux-generic-amd64.tar.xz
+
+# Download precompiled static Linux wkhtmltopdf
+curl -L -o bin/wkhtmltopdf.tar.xz \
+  https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox-0.12.6-1_linux-generic-amd64.tar.xz
+
+# Extract only the binary into bin/
 tar -xf bin/wkhtmltopdf.tar.xz --strip-components=2 -C bin wkhtmltox/bin/wkhtmltopdf
+
+# Make it executable
 chmod +x bin/wkhtmltopdf
+
+# Cleanup archive
 rm bin/wkhtmltopdf.tar.xz
