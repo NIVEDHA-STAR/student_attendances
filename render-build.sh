e4498b1241b9309b8e8fee8cda706916b4726833
NIVEDHA-STAR <muthunivedha135@gamil.com>
2025-07-12 13:54:28 +0530

    Removed render-build.sh to fix Render deploy

--- render-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y wget gnupg
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-apt install -y ./wkhtmltox_0.12.6-1.bionic_amd64.deb
