7d75b3b7eca676ec6c7ab6a2d1142475e1b14647
NIVEDHA-STAR <muthunivedha135@gamil.com>
2025-07-12 13:44:08 +0530

    Final render deploy version

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-
-# Download wkhtmltopdf precompiled binary
-mkdir -p bin
-curl -L -o bin/wkhtmltopdf https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltopdf_0.12.6-1.buster_amd64.deb
-
-# Install .deb and extract binary
-sudo apt-get update && sudo apt-get install -y gdebi-core
-sudo gdebi -n bin/wkhtmltopdf
-
-# Make it executable
-chmod +x /usr/local/bin/wkhtmltopdf
+apt-get update
+apt-get install -y wget gnupg
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+apt install -y ./wkhtmltox_0.12.6-1.bionic_amd64.deb
